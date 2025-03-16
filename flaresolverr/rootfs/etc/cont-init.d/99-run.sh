2d1fcbab780de99fa5800dc35657f7fdf449e5e7
cosote <cosote@gmail.com>
2025-03-16 15:04:35 +0100

    Update 99-run.sh

--- flaresolverr/rootfs/etc/cont-init.d/99-run.sh
+++ flaresolverr/rootfs/etc/cont-init.d/99-run.sh
@@ -7,5 +7,5 @@ bashio::log.warning "Warning - minimum configuration recommended : 2 cpu cores a
 ##############
 # LAUNCH APP #
 ##############
-pip install -r /app/requirements.txt
+pip install -r /app/requirements.txt --break-system-packages
 python -u /app/flaresolverr.py
